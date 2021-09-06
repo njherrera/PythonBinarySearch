0ec86b2776aa85a0179605d4e47853bd24b98001
njherrera <nathan.j.herrera@gmail.com>
2021-09-06 16:42:12 -0700

    squashed one bug, uncovered another

--- ArraySearcher.py
+++ ArraySearcher.py
@@ -8,21 +8,20 @@ def binarySearch ( list, target):
     if target is list[0] and len(list) == 1:
         return target
         print("target found, value is: " + target)
-    elif target is list[len(list) // 2]:
+    elif target is list[(len(list) - 1) // 2]:
+        print("target value found!")
         return target
     elif len(list) == 1 and target != list[0]:
         print("target value not found in array")
         return
-    elif target < list[len(list) // 2]: # if the target is less than the middle value of the list, it must be left of middle
+    elif target < list[(len(list) - 1) // 2]: # if the target is less than the middle value of the list, it must be left of middle
         print("going left")
-        leftPoint = 0
-        rightPoint = len(list) // 2 - 1 // # with test input of [4,5], this sets right point as length of list // 2 (1) - 1 (0), which of course results in a list with nothing in it
-        binarySearch(list[leftPoint: rightPoint], target)
-    elif target > list[len(list) // 2]: # if target is greater than the middle value of the list, it must be right of middle
+        rightPoint = (len(list) - 1) // 2 - 1 # with test input of [4,5], this sets right point as length of list // 2 (1) - 1 (0), which of course results in a list with nothing in it
+        binarySearch(list[: rightPoint], target)
+    elif target > list[(len(list) - 1) // 2]: # if target is greater than the middle value of the list, it must be right of middle
         print("going right")
-        leftPoint = len(list) // 2 + 1 # set new left side to be one right of the middle value
-        rightPoint = len(list) # set new right side to be the existing right side
-        binarySearch(list[leftPoint :], target) # call the method again, this time with the new left and right sides
+        leftPoint = (len(list) - 1) // 2 + 1 # set new left side to be one right of the middle value
+        binarySearch(list[leftPoint:], target) # call the method again, this time with the new left and right sides
 
 testList = []
 testList.append(1)
@@ -30,4 +29,7 @@ testList.append(2)
 testList.append(3)
 testList.append(4)
 testList.append(5)
-binarySearch(testList, 4)
+testList.append(6)
+testList.append(7)
+testList.append(8)
+binarySearch(testList, 5)
