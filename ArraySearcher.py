0c73891a187ff25a91266abe00d9402e18e50ce4
njherrera <nathan.j.herrera@gmail.com>
2021-09-05 18:31:39 -0700

    tracked down source of a bug

--- ArraySearcher.py
+++ ArraySearcher.py
@@ -4,16 +4,22 @@
 # repeat until target is found, or there are no more indices left to search
 
 def binarySearch ( list, target):
-    if target is list[len(list) // 2]:
+    print(list)
+    if target is list[0] and len(list) == 1:
+        return target
+        print("target found, value is: " + target)
+    elif target is list[len(list) // 2]:
         return target
     elif len(list) == 1 and target != list[0]:
         print("target value not found in array")
         return
     elif target < list[len(list) // 2]: # if the target is less than the middle value of the list, it must be left of middle
+        print("going left")
         leftPoint = 0
-        rightPoint = len(list) // 2 - 1
+        rightPoint = len(list) // 2 - 1 // # with test input of [4,5], this sets right point as length of list // 2 (1) - 1 (0), which of course results in a list with nothing in it
         binarySearch(list[leftPoint: rightPoint], target)
     elif target > list[len(list) // 2]: # if target is greater than the middle value of the list, it must be right of middle
+        print("going right")
         leftPoint = len(list) // 2 + 1 # set new left side to be one right of the middle value
         rightPoint = len(list) # set new right side to be the existing right side
         binarySearch(list[leftPoint :], target) # call the method again, this time with the new left and right sides
